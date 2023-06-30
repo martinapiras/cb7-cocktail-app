7dd130413a7a8e7369ab27a0ecdfe2638266acaa
martinapiras <131855775+martinapiras@users.noreply.github.com>
2023-06-30 18:51:01 +0200

    fix: filters disappearing when clicking on drink

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import "./index.css";
 
-const Filters = ({ categoryList, setCategoryList }) => {
-  const [filterList, setFilterList] = useState(categoryList);
-
+const Filters = ({
+  categoryList,
+  setCategoryList,
+  filterList,
+  setFilterList,
+}) => {
   const onHandleCheck = (id) => {
     setFilterList((prev) =>
       prev.map((category) => {
